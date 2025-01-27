5e1058b179d23d060ac2a250ba076d532288eca9
Frederik-DTU <s164222@student.dtu.dk>
2025-01-27 21:16:57 +0100

    Update

---
+++ mnist_diffusion/submit_all_runtime_gpu.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 17:22:08 2024
+
+@author: fmry
+"""
+
+#%% Modules
+
+import numpy as np
+
+import os
+
+import time
+
+#%% Submit jobs
+
+def submit_job():
+    
+    os.system("bsub < submit_runtime.sh")
+    
+    return
+
+#%% Generate jobs
+
+def generate_job():
+
+    with open ('submit_runtime.sh', 'w') as rsh:
+        rsh.write(f'''\
+    #! /bin/bash
+    #BSUB -q gpuv100
+    #BSUB -J mnist_dm
+    #BSUB -n 4
+    #BSUB -gpu "num=1:mode=exclusive_process"
+    #BSUB -W 24:00
+    #BSUB -R "span[hosts=1]"
+    #BSUB -R "rusage[mem=10GB]"
+    #BSUB -u fmry@dtu.dk
+    #BSUB -env "LSB_JOB_REPORT_MAIL=N"
+    #BSUB -B
+    #BSUB -N
+    #BSUB -o sendmeemail/error_%J.out 
+    #BSUB -e sendmeemail/output_%J.err 
+    
+    module swap cuda/12.0
+    module swap cudnn/v8.9.1.23-prod-cuda-12.X
+    module swap python3/3.10.12
+    
+    python3 train_mnist.py
+    ''')
+    
+    return
+
+if __name__ == '__main__':
+    
+    generate_job()
+    submit_job()
---
+++ mnist_diffusion/submit_runtime.sh
@@ -0,0 +1,21 @@
+    #! /bin/bash
+    #BSUB -q gpuv100
+    #BSUB -J mnist_dm
+    #BSUB -n 4
+    #BSUB -gpu "num=1:mode=exclusive_process"
+    #BSUB -W 24:00
+    #BSUB -R "span[hosts=1]"
+    #BSUB -R "rusage[mem=10GB]"
+    #BSUB -u fmry@dtu.dk
+    #BSUB -env "LSB_JOB_REPORT_MAIL=N"
+    #BSUB -B
+    #BSUB -N
+    #BSUB -o sendmeemail/error_%J.out 
+    #BSUB -e sendmeemail/output_%J.err 
+    
+    module swap cuda/12.0
+    module swap cudnn/v8.9.1.23-prod-cuda-12.X
+    module swap python3/3.10.12
+    
+    python3 train_mnist.py
+    
